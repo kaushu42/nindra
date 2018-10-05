4ad7c6e7b2456bb0876c11a9df0834da70d2d95c
kaush <kaushu42@gmail.com>
2018-10-05 11:29:59 +0545

    Edited html

--- Harkabahadur/reply/views.py
+++ Harkabahadur/reply/views.py
@@ -8,6 +8,6 @@ def index(request):
     if request.method == 'GET':
         sensor_value = request.GET.get('sensor_value')
         if sensor_value is not None:
-            # return HttpResponse('You sent a value: ' + sensor_value)
-            return render(request, 'index.html', {'name': 'milan'})
-    return HttpResponse('You are at the reply homepage')
+            return HttpResponse('The data is \\r' + sensor_value + '\\r')
+    return render(request, 'index.html', {'name': 'milan'})
+    # return HttpResponse('You are at the reply homepage')
