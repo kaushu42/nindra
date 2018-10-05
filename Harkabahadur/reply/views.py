1413a3d1dcb22c862deaf6a55ccdf66565102400
Milan <072bex426.milan@pcampus.edu.np>
2018-10-05 20:01:48 +0545

    Second Point

--- Harkabahadur/reply/views.py
+++ Harkabahadur/reply/views.py
@@ -9,5 +9,5 @@ def index(request):
         sensor_value = request.GET.get('sensor_value')
         if sensor_value is not None:
             return HttpResponse('The data is \\r' + sensor_value + '\\r')
+    
     return render(request, 'index.html', {'name': 'milan'})
-    # return HttpResponse('You are at the reply homepage')
